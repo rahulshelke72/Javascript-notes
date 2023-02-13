1ce91891b2175c30f2881bdcf930b89aeda1507b
rahulshelke72 <96787274+rahulshelke72@users.noreply.github.com>
2023-02-14 00:37:35 +0530

    fill array method

---
+++ Array_Methods/fill.js
@@ -0,0 +1,9 @@
+// fill method 
+// value , start , end 
+
+const myArray = new Array(10).fill(-1);
+console.log(myArray);
+
+const array1 = [1,2,3,4,5,6,7,8];
+array1.fill(0,2,5);
+console.log(array1);
