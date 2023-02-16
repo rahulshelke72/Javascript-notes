563955dd81e14da9cfdc8256e8f20e345305626f
rahul4172 <my email address used on my git repo>
2023-02-16 23:54:02 +0530

    Sets Iterables

---
+++ Array_Methods/splice.js
@@ -0,0 +1,18 @@
+// splice Method 
+// start , delete , insert
+
+const myArray = ['Item1' , 'Item2','Item3'];
+
+// delete 
+// const deletedItem = myArray.splice(1,1);
+// console.log(myArray);
+// console.log(deletedItem);
+
+// Insert 
+// myArray.splice(1,0,'Inserted Item');
+// console.log(myArray);
+
+// // Insert and Delete 
+const deletedItem = myArray.splice(1,2,"Inserted Item1" , "Inserted Item2");
+console.log(deletedItem);
+console.log(myArray);
