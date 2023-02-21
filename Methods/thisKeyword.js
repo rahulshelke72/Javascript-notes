7f7519bb63ed077ef8d6cbbb8edc9bed448fafe0
rahulshelke72 <96787274+rahulshelke72@users.noreply.github.com>
2023-02-21 23:55:50 +0530

    Methods in Javascript

---
+++ Methods/thisKeyword.js
@@ -0,0 +1,27 @@
+// Methods 
+// functions inside objects 
+
+function personalInfo() {
+    console.log(`person name is ${this.firstName} and age is ${this.age} `);
+}
+
+const person1 = {
+    firstName : "Harshit",
+    age : 31,
+    about : personalInfo
+}
+
+const person2 = {
+    firstName : "Ajay",
+    age : 21,
+    about : personalInfo
+}
+const person3 = {
+    firstName : "Bhagyesh",
+    age : 22,
+    about : personalInfo
+}
+
+person1.about();
+person2.about();
+person3.about(); 
