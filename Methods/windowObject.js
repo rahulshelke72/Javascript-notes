009266045822293bd0dc07dd3e464ca11fab9db4
rahul4172 <my email address used on my git repo>
2023-02-25 20:40:10 +0530

    Intro to Methods

---
+++ Methods/windowObject.js
@@ -0,0 +1,17 @@
+// Window Object 
+// use strict mode
+
+//console.log(this);
+// strict mode 
+
+"use strict";
+function myfunc(){
+    
+    console.log(this);
+}
+myfunc();
+
+
+// we can access function using window object 
+// e.g  window.myfunc()
+// by directly calling to function 
