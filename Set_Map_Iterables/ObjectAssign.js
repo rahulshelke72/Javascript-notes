a5993b02bfb1427f5417f6773bdefe9707ca93ad
rahul4172 <my email address used on my git repo>
2023-02-27 23:47:41 +0530

    Object assign

--- Set_Map_Iterables/ObjectAssign.js
+++ Set_Map_Iterables/ObjectAssign.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // clone using object.assign 
 // memory 
 
@@ -15,21 +14,3 @@ obj.key3 = "value3";
 console.log(obj3);
 console.log(obj);
 console.log(obj2);
-=======
-// clone using object.assign 
-// memory 
-
-const obj = {
-    key1 : "value1",
-    key2 : "value2"
-}
-
-const obj2 = {...obj};
-
-const obj3 = Object.assign({},obj);
-obj.key3 = "value3";
-
-console.log(obj3);
-console.log(obj);
-console.log(obj2);
->>>>>>> fac8e3c497dc0c64a6de2fc8e625707d10d8eec1
