2a7e28ba94f6f95c64ca6e5975080d0d7384eb29
rahul4172 <my email address used on my git repo>
2023-02-28 23:46:05 +0530

    arrow function

--- Methods/arrow_function.js
+++ Methods/arrow_function.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // arrow function 
 // In arrow function this always points one level upper than current object
 // consider in simple function this points to user1 object that time 
@@ -15,22 +14,4 @@ const user3 = {
 
 user3.about();
 // first prints window object then
-=======
-// arrow function 
-// In arrow function this always points one level upper than current object
-// consider in simple function this points to user1 object that time 
-// this in arrow function points to window object rather than user1 object
-
-const user3 = {
-    firstName : "abc" ,
-    age : 21,
-    about : ()=>{
-        console.log(this);
-        console.log(this.firstName,this.age);
-    }
-}
-
-user3.about();
-// first prints window object then
->>>>>>> be24bf50c3e097b5897b1c4a5f0183c5166126e8
 // It prints undefined undefined
