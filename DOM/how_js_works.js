ee8a9cbd9ebd92259d5ea191fccda3a686bcd053
rahul4172 <my email address used on my git repo>
2023-03-29 23:41:10 +0530

    js working

--- DOM/how_js_works.js
+++ DOM/how_js_works.js
@@ -16,4 +16,7 @@
 console.log(this);
 console.log(window);
 console.log(firstName);
+console.log(fullName);
 var firstName = "Banti";
+var lastName = "Sharma"
+var fullName = firstName + " " + lastName;
