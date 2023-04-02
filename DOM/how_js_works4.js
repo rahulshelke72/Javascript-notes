2e14badd55005bef92532b5d67883b9ffefdb0ef
rahul4172 <my email address used on my git repo>
2023-04-02 22:50:23 +0530

    js working

---
+++ DOM/how_js_works4.js
@@ -0,0 +1,16 @@
+// Uncaught ReferenceError:
+// Cannot access 'firstName' before initialization 
+
+// const variable without initialization gives uncaught error 
+
+// Uncaught ReferenceError : 
+// firstName is not defined 
+
+// console.log(firstName);
+
+// console.log(typeof firstName);
+// let firstName;
+// console.log(firstName);
+
+let firstName  = "Ajax";
+
