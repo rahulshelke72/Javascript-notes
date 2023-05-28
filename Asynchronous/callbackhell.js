6b94d12fd509233820803f9d50a2d04c0f47f64d
rahul4172 <my email address used on my git repo>
2023-05-28 22:38:58 +0530

    callback hell

--- Asynchronous/callbackhell.js
+++ Asynchronous/callbackhell.js
@@ -1,3 +1,35 @@
 // callbacks , callback hell , pyramid dom
+// asynchronous programming
+
+const heading1 = document.querySelector(".heading1");
+const heading2 = document.querySelector(".heading2");
+setTimeout(()=>{
+    heading1.textContent = "Heading 1";
+    heading1.style.color = "violet";
+    setTimeout(()=>{
+        heading2.textContent = "Heading 2";
+        heading2.style.color = "purple";
+    },1000)
+},1000)
+
+
+const heading3 = document.querySelector(".heading3");
+const heading4 = document.querySelector(".heading4");
+const heading5 = document.querySelector(".heading5");
+const heading6 = document.querySelector(".heading6");
+const heading7 = document.querySelector(".heading7");
+
+// Text      Delay   Color 
+
+// one       1s       Violet
+// two       2s       purple
+// three     2s       red
+// four      1s       Pink 
+// five      2s       green
+// six       3s       blue
+// seven     1s       brown 
+
+
 
 console.log("Hello World");
+
