0536efc88770aa06e90238f239a7d67ebc5b4cdb
Rahul <rahulshelke4172@gmail.com>
2023-07-03 23:08:20 +0530

    Asynchronous Js

---
+++ Asynchronous/callbackFunctions.js
@@ -0,0 +1,28 @@
+function makeUppercase(value){
+    console.log(value.toUpperCase());
+}
+
+//makeUppercase('peter');
+
+function reverseString(value){
+    console.log(value.split('').reverse().join(''));
+}
+
+function handleName(name , cb){
+    const fullName = `${name} smith`;
+    cb(fullName);
+    // some more logic
+}
+
+// handleName('peter',makeUppercase);
+// handleName('peter',reverseString)
+
+handleName('susan',(value)=> console.log(value));
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click',function (){
+    console.log('hello world');
+})
+
+// array methods , setTimeout , event listeners etc
