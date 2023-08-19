71961eefbfa8f7ea435c22e494da5cc0396bc1e5
Rahul <rahulshelke4172@gmail.com>
2023-08-19 23:21:07 +0530

    arrays.form , fetch error

---
+++ Asynchronous/Fetch_Error.js
@@ -0,0 +1,23 @@
+// Javascript Nuggests 
+// Fetch Errors 
+// Only throws an error if cannot resolve.
+// Error Response still a response (400 - 500)
+
+const url = 'https://www.course-api.com/react-tours-project';
+
+const getTours = async ()=>{
+    try{
+        const resp = await fetch(url)
+        if(!resp.ok){
+            const msg = `There was an error "${resp.status}  ${resp.statusText}"`
+            throw new Error(msg)
+        }
+        const tours = await resp.json()
+        console.log(tours);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+const btn = document.querySelector('.btn')
+btn.addEventListener('click',getTours)
